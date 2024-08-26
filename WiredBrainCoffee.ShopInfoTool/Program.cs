19f2f6434c0dca43d65271438811b8c2044e9757
SamyakManandhar <sammy.manandhar2004@gmail.com>
2024-08-26 13:04:56 +0100

    Implement shops command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,16 +1,31 @@
-﻿Console.WriteLine("--------------------------------------");
+﻿using WiredBrainCoffee.ShopInfoTool.Data;
+
+Console.WriteLine("--------------------------------------");
 Console.WriteLine(" Wired Brain Coffee - Shop Info Tool");
 Console.WriteLine("--------------------------------------");
 Console.WriteLine();
 Console.WriteLine("Commands:");
 Console.WriteLine("  quit   exit  application");
+Console.WriteLine("  shops   show  coffeeshops");
+
 
 while (true)
 {
     var command = Console.ReadLine();
-    if(string.Equals("quit",command, StringComparison.OrdinalIgnoreCase))
+    if (string.Equals("quit", command, StringComparison.OrdinalIgnoreCase))
     {
         break;
     }
+    else if (string.Equals("shops", command, StringComparison.OrdinalIgnoreCase))
+    {
+        var dataProvider = new CoffeeShopDataProvider();
+        var coffeeShops = dataProvider.LoadShops();
+
+        foreach (var coffeeShop in coffeeShops)
+        {
+            Console.WriteLine(coffeeShops);
+        }
+    }
+
 }
 
